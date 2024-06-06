22edcefed97d6fb55f1c308d4bedca65872aa6cd
Younsoo Tom <tomandlim@gmail.com>
2024-06-06 18:44:38 +0900

    feat: update headless redirector and cleanup dir (#3)
    Co-authored-by: deemo <deemo@cinamon.io>

--- bat/redirector_headless.bat
+++ bat/redirector_headless.bat
@@ -4,8 +4,8 @@
 
 echo Run Automation Redirection
 
-cd /d C:\Program Files\Epic Games\UE_5.3\Engine\Binaries\Win64\
+cd /d D:\unreal\base\UE_5.3\Engine\Binaries\Win64
 
-UnrealEditor.exe D:\CINEVStudio\CINEVStudio.uproject -run=ResavePackages -fixupredirects -autocheckout -projectonly -unattended
+UnrealEditor.exe E:\CINEVStudio\CINEVStudio\CINEVStudio.uproject -run=ResavePackages -fixupredirects -autocheckout -projectonly -unattended
 
 exit
