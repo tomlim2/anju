3f56c1474acad3ade1e8094b3b958e7fbe1c3e96
deemo <deemo@cinamon.io>
2024-07-19 09:05:39 +0900

    feat: add set metadata tag

--- python/testbed.py
+++ python/testbed.py
@@ -1,1 +1,22 @@
-print("Hihi".lower())
+from Lib import __lib_topaz__ as topaz
+import unreal 
+
+temp = unreal.EditorUtilityLibrary.get_selected_assets()[0]
+
+print(temp)
+
+ctrl : unreal.AnimationDataController = temp.controller
+
+print(ctrl)
+
+frames= unreal.FrameNumber(300)
+f_t0 = unreal.FrameNumber(3951)
+f_t1 = unreal.FrameNumber(4251)
+
+
+
+t1f = 131.69
+t2f = 141.59
+sub = t2f - t1f
+
+ctrl.set_number_of_frames(sub,t1f, t2f)
