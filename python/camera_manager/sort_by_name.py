efa12965ac3920786061e9bd4dfe60b779737376
deemo <deemo@cinamon.io>
2024-10-24 11:13:27 +0900

    feat: separate codes for managing data assets

--- python/camera_manager/sort_by_name.py
+++ python/camera_manager/sort_by_name.py
@@ -1,14 +1,10 @@
 import unreal
 
 cameras: list[unreal.CameraActor]
-sorted_cameras = sorted(cameras, key=lambda camera: (int( camera.get_actor_label().lower().partition('photospot')[2].partition('_')[0]), int( camera.get_actor_label().partition('_')[2]) if '_' in  camera.get_actor_label() else 0))
-
-# sorted_cameras = sorted(cameras, key=lambda camera: camera.get_actor_label())
-
-# testArray = ['Photospot1', 'Photospot10_1', 'Photospot10_2','Photospot10_3', 'Photospot2', 'Photospot3', 'Photospot4', 'Photospot5', 'Photospot6', 'Photospot7', 'Photospot8', 'Photospot9']
-
-# sortedArray = sorted(testArray, key=lambda x: (int(x.partition('Photospot')[2].partition('_')[0]), int(x.partition('_')[2]) if '_' in x else 0))
-# print(sortedArray)
+sorted_cameras = sorted(cameras, key=lambda camera: (
+    int(camera.get_actor_label().lower().partition('photospot')[2].partition('_')[0] or
+        camera.get_actor_label().lower().partition('samplespot')[2].partition('_')[0] or 0),
+    int(camera.get_actor_label().partition('_')[2] or 0) if '_' in camera.get_actor_label() else 0
+))
 
 
-# sorted(testArray, key=lambda camera: (int( camera.get_actor_label().partition('Photospot')[2].partition('_')[0]), int( camera.get_actor_label().partition('_')[2]) if '_' in  camera.get_actor_label() else 0))
