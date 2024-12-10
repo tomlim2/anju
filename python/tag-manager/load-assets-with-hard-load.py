73a22b7019d9eda5cc50c4ef35f27325291d909c
Younsoo Tom <tomandlim@gmail.com>
2024-12-10 11:38:28 +0900

    add script for hard load assets on memory for metatag (#32)
    * refactor: add asset hard load flow for metatag
    
    ---------
    
    Co-authored-by: deemo <deemo@cinamon.io>

---
+++ python/tag-manager/load-assets-with-hard-load.py
@@ -0,0 +1,32 @@
+import unreal
+
+def load_assets(folder_path, does_hard_load):
+    asset_registry = unreal.AssetRegistryHelpers.get_asset_registry()
+    assets = asset_registry.get_assets_by_path(folder_path, recursive=True)
+    error_message: str
+    if assets is not None:
+        static_meshes = []
+        for asset_data in assets:
+            asset_data_class: unreal.Class = asset_data.get_class()
+            if not asset_data_class:
+                continue
+            class_name = asset_data_class.get_name()
+            is_static_mesh = class_name == 'StaticMesh'
+            if does_hard_load:
+                print(f"Found asset: {asset_data.package_name} - {class_name}")
+            if is_static_mesh:
+                static_meshes.append(asset_data)
+        error_message = f"{folder_path}의 스태틱 매시 {len(static_meshes)}개"
+        return error_message
+    else:
+        error_message = f"{folder_path}에서 에셋을 찾을 수 없습니다"
+        return error_message
+
+folder_pathes = ["/Game/CineProps", "/Game/Market_Purchase"]
+error_messages = []
+does_hard_load: bool
+
+for folder_path in folder_pathes:
+    result = load_assets(folder_path, does_hard_load)
+    print(result)
+    error_messages.append(unreal.Name(result))
