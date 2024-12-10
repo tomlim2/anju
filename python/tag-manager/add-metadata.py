d740f833ea02b0bd5317a608fb16599d00685b87
Younsoo Tom <tomandlim@gmail.com>
2024-12-10 16:09:36 +0900

    update script for tag manager (#33)
    * refactor: add asset hard load flow for metatag
    
    * refactor: load assets static mesh only hard
    
    * feat: enable changing key in add-metadata
    
    ---------
    
    Co-authored-by: deemo <deemo@cinamon.io>

--- python/tag-manager/add-metadata.py
+++ python/tag-manager/add-metadata.py
@@ -5,9 +5,12 @@ input_list = input_tags.strip('[]').split(',')
 
 output_list:list[list[str, str]] = [[]]
 selected_assets:list[unreal.Object] = unreal.EditorUtilityLibrary.get_selected_assets()
+error_message:str
+key_target: str
 
 if len(selected_assets) == 0:
-    print('No assets selected')
+    error_message = 'No assets selected'
+
     quit()
 
 for asset in selected_assets:
@@ -17,24 +20,24 @@ for asset in selected_assets:
     has_tag = False
     does_metadata_exist = len(all_metadata) > 0
 
-    print(does_metadata_exist, 'all_metadata')
-
     if does_metadata_exist == False:
-        new_key_value = ['#', input_tags]
+        new_key_value.append(key_target)
+        new_key_value.append(input_tags)
     else:
         for tag_name, value in all_metadata.items():
-            if tag_name == '#':
+            if tag_name == key_target:
                 has_tag = True
                 value_array = value.strip('[]').split(',')
                 for item in input_list:
                     if item not in value:
                         value_array.append(item)
-                # print(value_array)
                 value_string = '[' + ','.join(value_array) + ']'
-                new_key_value = ['#', value_string]
-                print(value_string)
+                new_key_value.append(key_target)
+                new_key_value.append(value_string)
             if has_tag == False:
-                new_key_value = ['#', input_tags]
-    print(f'new_key_value[0] is {new_key_value[0]}', new_key_value[1])
+                new_key_value.append(key_target)
+                new_key_value.append(input_tags)
+    error_message = f'new_key_value[0] is {new_key_value[0]}', new_key_value[1]
+
     unreal.EditorAssetLibrary.set_metadata_tag(asset, new_key_value[0], new_key_value[1])
     unreal.EditorAssetLibrary.save_asset(asset.get_path_name())
