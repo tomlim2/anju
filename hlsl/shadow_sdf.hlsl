bdbd9b35557fdb51eaee2f776bf69076c05e0970
deemo <deemo@cinamon.io>
2025-11-13 16:58:15 +0900

    feature: add api test
    remove vrms
    
    remove vrm

--- hlsl/shadow_sdf.hlsl
+++ hlsl/shadow_sdf.hlsl
@@ -1,28 +1,35 @@
 float dotFB = dot(LightVector, Head_Forward_Vector);
 float dotLR = dot(LightVector, Head_Right_Vector);
 
-float finalSDF = 0.0;
+float finalSDF;
 
-if(dotLR > 0.0)
+// Right side lighting
+if (dotLR > 0.0)
 {
-if (dotFB > 0.0)
-{
-finalSDF = mad(dotFB, 0.85, 0.15);
+    // Front lighting
+    if (dotFB > 0.0)
+    {
+        finalSDF = mad(dotFB, 1.0, 0.4);
+    }
+    // Back lighting
+    else
+    {
+        finalSDF = max(0.0, mad(dotFB, 1.0, 0.4));
+    }
 }
-if (dotFB <= 0.0)
+// Left side lighting
+else
 {
-finalSDF = max(0.0, mad(dotFB, 0.2, 0.15));
-}
-}
-else 
-{
-if (dotFB > 0.0)
-{
-finalSDF = mad(dotFB, -0.92, -0.85);
-}
-if (dotFB <= 0.0)
-{
-finalSDF = min(0.0, mad(dotFB, -0.8, -0.75)); 
+    // Front lighting
+    if (dotFB > 0.0)
+    {
+        finalSDF = mad(dotFB, -1.0, -0.6);
+    }
+    // Back lighting
+    else
+    {
+        finalSDF = min(0.0, mad(dotFB, -1.0, -0.6));
+    }
 }
 
 return finalSDF;
