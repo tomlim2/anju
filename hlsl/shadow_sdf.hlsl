a744db27d6a94f3df756c52d39dcb0d23fc82ac4
YounsooLim <tomandlim@gmail.com>
2025-11-25 21:00:04 +0900

    update shadow sdf logic

--- hlsl/shadow_sdf.hlsl
+++ hlsl/shadow_sdf.hlsl
@@ -2,14 +2,35 @@ float dotFB = dot(LightVector, Head_Forward_Vector);
 float dotLR = dot(LightVector, Head_Right_Vector);
 
 float finalSDF;
+float halfPoint = 0.5;
 
-if (dotFB > 0.0)
+// Right side lighting
+if (dotLR > 0.0)
 {
-    finalSDF = mad(dotFB, -1.0, -0.6);
+    // Front lighting
+    if (dotFB > 0.0)
+    {
+        finalSDF = mad(dotFB, 1.0, halfPoint);
+    }
+    // Back lighting
+    else
+    {
+        finalSDF = max(0.0, mad(dotFB, 1.0, halfPoint));
+    }
 }
-// Back lighting
+// Left side lighting
 else
 {
-    finalSDF = min(0.0, mad(dotFB, -1.0, -0.6));
+    // Front lighting
+    if (dotFB > 0.0)
+    {
+        finalSDF = mad(dotFB, -1.0, -1.0 + halfPoint);
+    }
+    // Back lighting
+    else
+    {
+        finalSDF = min(0.0, mad(dotFB, -1.0, -1.0 + halfPoint));
+    }
 }
+
 return finalSDF;
