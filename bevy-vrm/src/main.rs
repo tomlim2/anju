0932bb50823a0be437ffb47425f92687c921f804
YounsooLim <tomandlim@gmail.com>
2026-03-19 18:33:21 +0900

    feat(bevy-vrm): camera preset views (1:front, 2:side, 3:top)

--- bevy-vrm/src/main.rs
+++ bevy-vrm/src/main.rs
@@ -164,6 +164,7 @@ fn main() {
             start_playback,
             toggle_bone_viz,
             draw_bone_viz,
+            camera_presets,
         ))
         .run();
 }
@@ -222,7 +223,7 @@ fn setup(
         },
     ));
 
-    log.push("[INIT] O: VRM | F: FBX | G: bones | F3: stats | F4: log");
+    log.push("[INIT] O:VRM F:FBX G:bones 1:front 2:side 3:top F3:stats F4:log");
 }
 
 fn open_file_dialog(
@@ -700,6 +701,41 @@ fn update_debug_panel(
         mesh_count, image_count, image_mb, material_count,
         process_mem,
     );
+}
+
+fn camera_presets(
+    input: Res<ButtonInput<KeyCode>>,
+    mut camera_q: Query<(&mut Transform, &mut PanOrbitCamera)>,
+) {
+    let Some((mut tf, mut orbit)) = camera_q.iter_mut().next() else { return; };
+
+    let focus = Vec3::new(0.0, 1.0, 0.0);
+    let dist = 3.0;
+
+    if input.just_pressed(KeyCode::Digit1) {
+        // Front view: camera at +Z looking at character
+        *tf = Transform::from_xyz(0.0, 1.0, dist).looking_at(focus, Vec3::Y);
+        orbit.focus = focus;
+        orbit.radius = Some(dist);
+        orbit.yaw = Some(0.0);
+        orbit.pitch = Some(0.0);
+    }
+    if input.just_pressed(KeyCode::Digit2) {
+        // Side view: camera at +X
+        *tf = Transform::from_xyz(dist, 1.0, 0.0).looking_at(focus, Vec3::Y);
+        orbit.focus = focus;
+        orbit.radius = Some(dist);
+        orbit.yaw = Some(std::f32::consts::FRAC_PI_2);
+        orbit.pitch = Some(0.0);
+    }
+    if input.just_pressed(KeyCode::Digit3) {
+        // Top view: camera above
+        *tf = Transform::from_xyz(0.0, dist + 1.0, 0.01).looking_at(focus, Vec3::Z);
+        orbit.focus = focus;
+        orbit.radius = Some(dist);
+        orbit.yaw = Some(0.0);
+        orbit.pitch = Some(-std::f32::consts::FRAC_PI_2);
+    }
 }
 
 fn toggle_bone_viz(
